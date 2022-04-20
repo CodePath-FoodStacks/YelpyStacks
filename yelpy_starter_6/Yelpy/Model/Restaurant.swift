ef0a5ae0f50cdfa9e484716a49e3686c1c3838d4
Rafael Eduardo Lintao <relintao@gmail.com>
2022-04-20 10:26:42 -0700

    adding yelpy files

---
+++ yelpy_starter_6/Yelpy/Model/Restaurant.swift
@@ -0,0 +1,51 @@
+//
+//  Restaurant.swift
+//  Yelpy
+//
+//  Created by Memo on 5/21/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Lab 6: TODO: add MapKit framework --> import MapKit
+
+
+class Restaurant {
+    
+
+    var imageURL: URL?
+    var url: URL?
+    var name: String
+    var mainCategory: String
+    var phone: String
+    var rating: Double
+    var reviews: Int
+    var coordinates: [String:Double]
+    
+    
+    // MARK: Lab 6:  Refactor Restaurant Model
+    
+    
+
+    init(dict: [String: Any]) {
+        imageURL = URL(string: dict["image_url"] as! String)
+        name = dict["name"] as! String
+        rating = dict["rating"] as! Double
+        reviews = dict["review_count"] as! Int
+        phone = dict["display_phone"] as! String
+        url = URL(string: dict["url"] as! String)
+        mainCategory = Restaurant.getMainCategory(dict: dict)
+        
+        coordinates = dict["coordinates"] as! [String:Double]
+    }
+    
+    // Helper function to get First category from restaurant
+    static func getMainCategory(dict: [String:Any]) -> String {
+        let categories = dict["categories"] as! [[String: Any]]
+        return categories[0]["title"] as! String
+    }
+
+    
+}
